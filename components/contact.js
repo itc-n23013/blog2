4e283c12e786b09502202e0e0dc667c11c99951d
taiyou kuwae <n23013@std.it-college.ac.jp>
2024-01-26 00:42:16 +0000

    修正

--- components/contact.js
+++ components/contact.js
@@ -3,7 +3,7 @@ import styles from 'styles/contact.module.css'
 const Contact = () => {
   return (
     <div className={styles.stack}>
-      <h3 classname={styles.heading}>Contact</h3>
+      <h3 className={styles.heading}>Contact</h3>
       <address>cube@web.mail.address</address>
     </div>
   )
