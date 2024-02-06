86888bc57cab44b6527f7548a4755a6395ce779e
taiyou kuwae <n23013@std.it-college.ac.jp>
2024-02-06 07:11:15 +0000

    9.2 p252~253 finished
    Dynamic Routesで記事ページを生成する

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -15,7 +15,7 @@ import Image from 'next/image'
 import { getPlaiceholder } from 'plaiceholder'
 import { eyecatchLocal } from 'lib/constants'
 
-const Schedule = ({
+const Post = ({
   title,
   publish,
   content,
@@ -64,10 +64,17 @@ const Schedule = ({
     </Container>
   )
 }
-export default Schedule
+export default Post
 
-export const getStaticProps = async () => {
-  const slug = 'micro'
+export const getStaticPaths = async () => {
+  return {
+    paths: ['/blog/schedule', '/blog/music', '/blog/micro'],
+    fallback: false
+  }
+}
+
+export const getStaticProps = async context => {
+  const slug = context.params.slug
 
   const post = await getPostBySlug(slug)
 
